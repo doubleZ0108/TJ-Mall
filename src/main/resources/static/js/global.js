1cad7486c00714479a735631cad958903b23e6e4
double Z <doubleZ0108@163.com>
2020-05-23 20:40:44 +0800

    front: hero + navigator

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,3 +1,5 @@
+let TONGJI_IMG_MAX = 12;    // 总图标数
+
 /**
     $('container')
 
@@ -21,4 +23,18 @@ function setStyle(obj, css){
     for(let atr in css){
         obj.style[atr] = css[atr];
     }
+}
+
+function getRandomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,26 @@
 window.onload = function () {
-    console.log("hello world");
+    initNavigator();
+    initHero();
 };
+
+function initNavigator(){
+    let navigator = $('navigator');
+    document.addEventListener('scroll', function () {
+    if(self.pageYOffset > 50) {
+        navigator.classList.add('NavigationScrolled');
+    } else {
+        navigator.classList.remove('NavigationScrolled');
+    }
+    });
+}
+
+function initHero(){
+    let hero = $('hero');
+    
+    setInterval(function(){
+        let index = getRandomNum(1, 12);
+        console.log(index);
+        let index_str = index < 10 ? '0' + index.toString() : index.toString();
+        hero.style.backgroundImage = 'url("../img/tongji/bg/' + index_str + '.png")';
+    }, 10000);
+}
