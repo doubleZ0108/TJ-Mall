69e1ecd2320a97501e9231d6aa496870c70f54ab
double Z <doubleZ0108@163.com>
2020-05-24 23:36:28 +0800

    front: change to json-drived pattern

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     initNavigation();
     initHero();
+
+    initSocialMedia();
 }
 
 function initNavigation(){
@@ -22,13 +24,19 @@ function initNavigationList(){
 
     readJson("navigator.json")
         .then(navigatorObj => {
-            // console.log(navigatorObj["navigator"]);
             navigatorObj["navigator"].forEach(function(elem){
-                console.log(elem);
+                let navigationLi = $c('li');
+                let navigationA = $c('a');
+                navigationA.href = elem.href;
+                navigationA.title = elem.title;
+                navigationA.innerHTML = elem.content;
+                navigationLi.appendChild(navigationA);
+                navigationList.appendChild(navigationLi);
             });
         })
         .catch(error => console.log(error));
 }
+
 
 function initHero(){
     let hero = $('hero');
@@ -38,4 +46,28 @@ function initHero(){
         let index_str = index < 10 ? '0' + index.toString() : index.toString();
         hero.style.backgroundImage = 'url("../img/tongji/bg/' + index_str + '.png")';
     }, 10000);
+}
+
+function initSocialMedia(){
+    let socialMedia = $('SocialMediaGroup');
+    readJson("socialmedia.json")
+        .then(socialmediaObj => {
+            socialmediaObj["socialmedia"].forEach(function(elem){
+                let socialmediaDiv = $c('div');
+                socialmediaDiv.classList.add("SocialMediaIcon");
+                let socialmediaA = $c('a');
+                socialmediaA.href = elem.href;
+                socialmediaA.title = elem.title;
+                socialmediaA.target = "_blank";
+                let socialmediaI = $c('i');
+                socialmediaI.classList.add("iconfont");
+                socialmediaI.classList.add(elem.class);
+                socialmediaI.style.color = elem.color;
+
+                socialmediaA.appendChild(socialmediaI);
+                socialmediaDiv.appendChild(socialmediaA);
+                socialMedia.appendChild(socialmediaDiv);
+            });
+        })
+        .catch(error => console.log(error));
 }
