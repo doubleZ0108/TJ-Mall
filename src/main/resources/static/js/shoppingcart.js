b1d8327af9c05e7b868490091bce89173c2337f5
double Z <doubleZ0108@163.com>
2020-05-28 11:43:11 +0800

    front: shopping cart skeleton

---
+++ src/main/resources/static/js/shoppingcart.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    initNavigation();
+    initShoppingCart();
+};
+
+function initShoppingCart() {
+    let hero = $('hero');
+    hero.style.backgroundImage = 'url("../img/tongji/bg/' + '08' + '.png")';
+}
